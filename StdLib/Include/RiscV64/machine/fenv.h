c76990fd6c9a439bde4777fd18a9a98d304f8335
Icenowy Zheng <zhengxingda@iscas.ac.cn>
2026-06-05 01:36:10 +0800

    edk2-libc: add RISCV64 support
    - The fenv.h, arith.h, gd_qnan.h header files are retrieved from NetBSD,
      with fenv.h adapted to prevent __BIT() macro usage.
    - Other new source files are based on ones from AARCH64.
    
    Signed-off-by: Icenowy Zheng <zhengxingda@iscas.ac.cn>

---
+++ StdLib/Include/RiscV64/machine/fenv.h
@@ -0,0 +1,36 @@
+/*	$NetBSD: fenv.h,v 1.5 2024/05/12 20:04:12 riastradh Exp $	*/
+
+/*
+ * Based on ieeefp.h written by J.T. Conklin, Apr 28, 1995
+ * Public domain.
+ */
+
+#ifndef _RISCV_FENV_H_
+#define _RISCV_FENV_H_
+
+typedef int fenv_t;		/* FPSCR */
+typedef int fexcept_t;
+
+#define	FE_INEXACT	0x01	/* Result inexact */
+#define	FE_UNDERFLOW	0x02	/* Result underflowed */
+#define	FE_OVERFLOW	0x04	/* Result overflowed */
+#define	FE_DIVBYZERO	0x08	/* divide-by-zero */
+#define	FE_INVALID	0x10	/* Result invalid */
+
+#define	FE_ALL_EXCEPT	\
+    (FE_INEXACT | FE_UNDERFLOW | FE_OVERFLOW | FE_DIVBYZERO | FE_INVALID)
+
+#define	FE_TONEAREST	0	/* round to nearest representable number */
+#define	FE_TOWARDZERO	1	/* round to zero (truncate) */
+#define	FE_DOWNWARD	2	/* round toward negative infinity */
+#define	FE_UPWARD	3	/* round toward positive infinity */
+
+__BEGIN_DECLS
+
+/* Default floating-point environment */
+extern const fenv_t	__fe_dfl_env;
+#define FE_DFL_ENV	(&__fe_dfl_env)
+
+__END_DECLS
+
+#endif /* _RISCV_FENV_H_ */
